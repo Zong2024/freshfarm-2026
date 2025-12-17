a252ee3b500bd34cb60aab70a7d5f5615f4cac48
zong <Zong20021107@gmail.com>
2025-12-17 20:59:25 +0800

    feat: 新增 Bootstrap JS 依賴

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './assets/scss/all.scss'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 createRoot(document.getElementById('root')).render(<App />)
