c20bbbca0855904795fa199ef0ae45d03eeca801
zong <Zong20021107@gmail.com>
2025-12-18 12:47:03 +0800

    feat(Navbar): 更新導航欄元件，使用 NavLink 取代 Link、並修改路由位置

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,74 +1,93 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import logo from '../assets/images/logo.png'
+import { useState } from 'react'
 const Navbar = () => {
+	// eslint-disable-next-line no-unused-vars
+	const [isSignIn, setIsSignIn] = useState(false) //還未新增邏輯
 	return (
-		<>
-			<div className="nav-box">
-				<div className="container">
-					<div id="nav" className="py-2 py-lg-4 px-3 px-lg-7 navbar-rounded mt-lg-8 bg-white">
-						<nav className="navbar navbar-expand-lg header-nav">
-							<div className="container p-0">
-								<Link className="navbar-brand py-0" to="/">
-									<img src={logo} alt="首頁" className="nav-logo object-fit-cover" />
-								</Link>
-								<button
-									className="navbar-toggler border-0 bg-gray-100 p-2"
-									type="button"
-									data-bs-toggle="offcanvas"
-									data-bs-target="#offcanvasNavbar"
-									aria-label="開啟選單"
-								>
-									<span className="navbar-toggler-icon"></span>
-								</button>
-								<div
-									className="offcanvas offcanvas-end bg-white"
-									// tabindex="-1"
-									id="offcanvasNavbar"
-									aria-labelledby="offcanvasNavbarLabel"
-								>
-									<div className="offcanvas-header px-3 py-2 p-lg-0">
-										<Link to="/" className="offcanvas-title" id="offcanvasNavbarLabel">
-											<img
-												src={logo}
-												alt="logo"
-												className="object-fit-cover"
-												style={{ width: '64px', height: '53px' }}
-											/>
-										</Link>
-										<button
-											type="button"
-											className="btn-close me-0 bg-gray-100"
-											data-bs-dismiss="offcanvas"
-											aria-label="關閉選單"
-										></button>
+		<div className="nav-box">
+			<div className="container">
+				<div id="nav" className="py-2 py-lg-4 px-3 px-lg-7 navbar-rounded mt-lg-8 bg-white">
+					<nav className="navbar navbar-expand-lg header-nav">
+						<div className="container p-0">
+							<NavLink className="navbar-brand py-0" to="/">
+								<img src={logo} alt="首頁" className="nav-logo object-fit-cover" />
+							</NavLink>
+							<button
+								className="navbar-toggler border-0 bg-gray-100 p-2"
+								type="button"
+								data-bs-toggle="offcanvas"
+								data-bs-target="#offcanvasNavbar"
+								aria-label="開啟選單"
+							>
+								<span className="navbar-toggler-icon"></span>
+							</button>
+							<div
+								className="offcanvas offcanvas-end bg-white"
+								// tabindex="-1"
+								id="offcanvasNavbar"
+								aria-labelledby="offcanvasNavbarLabel"
+							>
+								<div className="offcanvas-header px-3 py-2 p-lg-0">
+									<div className="offcanvas-title" id="offcanvasNavbarLabel">
+										<img
+											src={logo}
+											alt="logo"
+											className="object-fit-cover"
+											style={{ width: '64px', height: '53px' }}
+										/>
 									</div>
-									<div className="offcanvas-body px-6 py-6 p-lg-0">
-										<ul className="navbar-nav ms-auto">
-											<li className="nav-item pe-lg-8 pb-5 pb-lg-0  nav-bottom-line">
-												<Link className="nav-link text-lg-center p-0" aria-current="page" to="/">
-													<span className="fs-lg-4 fs-5 fw-bold">首頁</span>
-													<p className="text-gray-300 ">Home</p>
-												</Link>
-											</li>
-											<li className="nav-item py-5 py-lg-0 nav-bottom-line pe-lg-8 ">
-												<Link className="nav-link text-lg-center  p-0" to="/products">
-													<span className="fs-lg-4 fs-5 fw-bold">產地直送</span>
-													<p className="text-gray-300">Delivery</p>
-												</Link>
-											</li>
-											<li className="nav-item py-5 py-lg-0 nav-bottom-line pe-lg-8">
-												<Link className="nav-link text-lg-center p-0" to="/checkout">
-													<span className="fs-lg-4 fs-5 fw-bold">購物車</span>
-													<p className="text-gray-300">Cart</p>
-												</Link>
-											</li>
-											<li className="nav-item py-5 py-lg-0">
-												{/* <!-- 未登入 --> */}
-												{/* <!-- <a className="nav-link text-lg-center  p-0" href="#">
-                                                        <span className="fs-lg-4 fs-5 fw-bold">登入/註冊</span>
-                                                        <p className="text-gray-300">Sign in</p>
-                                                    </a> --> */}
-												{/* <!-- 已登入 --> */}
+									<button
+										type="button"
+										className="btn-close me-0 bg-gray-100"
+										data-bs-dismiss="offcanvas"
+										aria-label="關閉選單"
+									></button>
+								</div>
+								<div className="offcanvas-body px-6 py-6 p-lg-0">
+									<ul className="navbar-nav ms-auto">
+										<li className="nav-item pe-lg-8 pb-5 pb-lg-0  nav-bottom-line">
+											<NavLink
+												className={({ isActive }) =>
+													`nav-link text-lg-center p-0 ${isActive ? 'active' : ''}`
+												}
+												to="/"
+												end
+												aria-current="page"
+											>
+												<span className="fs-lg-4 fs-5 fw-bold">首頁</span>
+												<p className="text-gray-300 ">Home</p>
+											</NavLink>
+										</li>
+										<li className="nav-item py-5 py-lg-0 nav-bottom-line pe-lg-8 ">
+											<NavLink
+												className={({ isActive }) =>
+													`nav-link text-lg-center p-0 ${isActive ? 'active' : ''}`
+												}
+												to="/products"
+											>
+												<span className="fs-lg-4 fs-5 fw-bold">產地直送</span>
+												<p className="text-gray-300">Delivery</p>
+											</NavLink>
+										</li>
+										<li className="nav-item py-5 py-lg-0 nav-bottom-line pe-lg-8">
+											<NavLink
+												className={({ isActive }) =>
+													`nav-link text-lg-center p-0 ${isActive ? 'active' : ''}`
+												}
+												to="/checkout"
+											>
+												<span className="fs-lg-4 fs-5 fw-bold">購物車</span>
+												<p className="text-gray-300">Cart</p>
+											</NavLink>
+										</li>
+										<li className="nav-item py-5 py-lg-0">
+											{isSignIn ? (
+												<NavLink className="nav-link text-lg-center  p-0" to="/sign_in">
+													<span className="fs-lg-4 fs-5 fw-bold">登入/註冊</span>
+													<p className="text-gray-300">Sign in</p>
+												</NavLink>
+											) : (
 												<button
 													className="nav-link dropdown-toggle p-0 w-100 d-flex"
 													type="button"
@@ -83,61 +102,60 @@ const Navbar = () => {
 														<span className="material-icons ">keyboard_arrow_right</span>
 													</div>
 												</button>
-												<ul className="dropdown-menu dropdown-menu-end border-0 rounded-3 dropdown-shadow mt-lg-6 p-4 ">
-													<h6 className="text-primary-400 mb-3 dropdown-top-line pt-4 pt-lg-0">
-														會員中心
-													</h6>
-													<li>
-														<Link
-															className="dropdown-item  me-2 mb-3 mb-lg-1"
-															to="/"
-															aria-hidden="true"
-														>
-															<span className="material-icons align-bottom me-2">
-																account_circle
-															</span>
-															我的帳戶
-														</Link>
-													</li>
-													<li>
-														<Link
-															className="dropdown-item  me-2 mb-3 mb-lg-1"
-															to="/"
-															aria-hidden="true"
-														>
-															<span className="material-icons align-bottom me-2">description</span>
-															訂單管理
-														</Link>
-													</li>
-													<li>
-														<Link
-															className="dropdown-item  me-2 mb-3 mb-lg-1"
-															to="/"
-															aria-hidden="true"
-														>
-															<span className="material-icons align-bottom me-2">
-																favorite_border
-															</span>
-															收藏清單
-														</Link>
-													</li>
-													<li>
-														<Link className="dropdown-item " to="/" aria-hidden="true">
-															<span className="material-icons align-bottom me-2">settings</span>
-															個人資料設定
-														</Link>
-													</li>
-												</ul>
-											</li>
-										</ul>
-									</div>
+											)}
+
+											<ul className="dropdown-menu dropdown-menu-end border-0 rounded-3 dropdown-shadow mt-lg-6 p-4 ">
+												<h6 className="text-primary-400 mb-3 dropdown-top-line pt-4 pt-lg-0">
+													會員中心
+												</h6>
+												<li>
+													<NavLink
+														className="dropdown-item  me-2 mb-3 mb-lg-1"
+														to="/user"
+														aria-hidden="true"
+													>
+														<span className="material-icons align-bottom me-2">account_circle</span>
+														我的帳戶
+													</NavLink>
+												</li>
+												<li>
+													<NavLink
+														className="dropdown-item  me-2 mb-3 mb-lg-1"
+														to="/orders"
+														aria-hidden="true"
+													>
+														<span className="material-icons align-bottom me-2">description</span>
+														訂單管理
+													</NavLink>
+												</li>
+												<li>
+													<NavLink
+														className="dropdown-item  me-2 mb-3 mb-lg-1"
+														to="/wishlist"
+														aria-hidden="true"
+													>
+														<span className="material-icons align-bottom me-2">
+															favorite_border
+														</span>
+														收藏清單
+													</NavLink>
+												</li>
+												<li>
+													<NavLink className="dropdown-item " to="/edit" aria-hidden="true">
+														<span className="material-icons align-bottom me-2">settings</span>
+														個人資料設定
+													</NavLink>
+												</li>
+											</ul>
+										</li>
+									</ul>
 								</div>
 							</div>
-						</nav>
-					</div>
+						</div>
+					</nav>
 				</div>
 			</div>
-		</>
+		</div>
 	)
 }
 
