f69f4608f66f9c8771db22857490250d782d42f8
zong <Zong20021107@gmail.com>
2025-12-18 11:37:50 +0800

    feat: 更新導航欄元件，改善可讀性與無障礙性

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
 					<div id="nav" className="py-2 py-lg-4 px-3 px-lg-7 navbar-rounded mt-lg-8 bg-white">
 						<nav className="navbar navbar-expand-lg header-nav">
 							<div className="container p-0">
-								<Link className="navbar-brand py-0" to="/" tabIndex="-1" aria-disabled="true">
-									<img src={logo} alt="logo" className="nav-logo object-fit-cover" />
+								<Link className="navbar-brand py-0" to="/">
+									<img src={logo} alt="首頁" className="nav-logo object-fit-cover" />
 								</Link>
 								<button
 									className="navbar-toggler border-0 bg-gray-100 p-2"
@@ -69,48 +69,61 @@ const Navbar = () => {
                                                         <p className="text-gray-300">Sign in</p>
                                                     </a> --> */}
 												{/* <!-- 已登入 --> */}
-												<div data-bs-toggle="dropdown" aria-expanded="false">
-													<div>
+												<button
+													className="nav-link dropdown-toggle p-0 w-100 d-flex"
+													type="button"
+													data-bs-toggle="dropdown"
+													aria-expanded="false"
+												>
+													<div className="text-start text-lg-center">
 														<span className="fs-lg-4 fs-5 fw-bold">用戶名稱</span>
-														<p className="text-gray-300 text-lg-center">Account</p>
+														<p className="text-gray-300 ">Account</p>
 													</div>
 													<div className="align-self-center d-lg-none ms-auto">
 														<span className="material-icons ">keyboard_arrow_right</span>
 													</div>
-												</div>
+												</button>
 												<ul className="dropdown-menu dropdown-menu-end border-0 rounded-3 dropdown-shadow mt-lg-6 p-4 ">
 													<h6 className="text-primary-400 mb-3 dropdown-top-line pt-4 pt-lg-0">
 														會員中心
 													</h6>
 													<li>
-														<Link className="dropdown-item  me-2 mb-3 mb-lg-1" to="/">
-															<span className="material-icons align-bottom me-2" aria-hidden="true">
+														<Link
+															className="dropdown-item  me-2 mb-3 mb-lg-1"
+															to="/"
+															aria-hidden="true"
+														>
+															<span className="material-icons align-bottom me-2">
 																account_circle
 															</span>
 															我的帳戶
 														</Link>
 													</li>
 													<li>
-														<Link className="dropdown-item  me-2 mb-3 mb-lg-1" to="/">
-															<span className="material-icons align-bottom me-2" aria-hidden="true">
-																description
-															</span>
+														<Link
+															className="dropdown-item  me-2 mb-3 mb-lg-1"
+															to="/"
+															aria-hidden="true"
+														>
+															<span className="material-icons align-bottom me-2">description</span>
 															訂單管理
 														</Link>
 													</li>
 													<li>
-														<Link className="dropdown-item  me-2 mb-3 mb-lg-1" to="/">
-															<span className="material-icons align-bottom me-2" aria-hidden="true">
+														<Link
+															className="dropdown-item  me-2 mb-3 mb-lg-1"
+															to="/"
+															aria-hidden="true"
+														>
+															<span className="material-icons align-bottom me-2">
 																favorite_border
 															</span>
 															收藏清單
 														</Link>
 													</li>
 													<li>
-														<Link className="dropdown-item " to="/">
-															<span className="material-icons align-bottom me-2" aria-hidden="true">
-																settings
-															</span>
+														<Link className="dropdown-item " to="/" aria-hidden="true">
+															<span className="material-icons align-bottom me-2">settings</span>
 															個人資料設定
 														</Link>
 													</li>
