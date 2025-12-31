c9e990f76c29d42cead23f743a246487dbe11d87
zong <Zong20021107@gmail.com>
2025-12-31 14:23:58 +0800

    refactor:重構SearchBar 元件

---
+++ src/components/input/SearchBar/SearchBar.jsx
@@ -0,0 +1,16 @@
+import clsx from 'clsx'
+import styles from './SearchBar.module.scss'
+const SearchBar = () => {
+	return (
+		<div className={clsx(styles.searchBox)}>
+			<div className={clsx(styles.customSearchBar)}>
+				<input type="text" className={clsx(styles.searchInput)} placeholder="請輸入商品名稱…" />
+				<button type="button" className={clsx(styles.searchBtn)}>
+					<span className={clsx('material-icons', styles.searchBtnIcon)}>search</span>
+				</button>
+			</div>
+		</div>
+	)
+}
+
+export default SearchBar
