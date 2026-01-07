7ac26cb14e078447d2c321bf4c3f52186788cbc5
zong <Zong20021107@gmail.com>
2026-01-07 12:36:47 +0800

    feat : quantity selector component

---
+++ src/components/input/QuantitySelector/QuantitySelector.jsx
@@ -0,0 +1,63 @@
+import { clsx } from 'clsx'
+import styles from './QuantitySelector.module.scss'
+const QuantitySelector = ({ value = 1, max = 15, onChange }) => {
+	const isMinusDisabled = value <= 1
+	const isPlusDisabled = value >= max
+
+	const handleInputChange = e => {
+		const val = e.target.value
+		return val
+	}
+	const handleUpdate = newValue => {
+		let safeValue = Math.max(1, newValue)
+		if (max) safeValue = Math.min(safeValue, max)
+
+		if (safeValue !== value) {
+			onChange(safeValue)
+		}
+	}
+
+	return (
+		<div className={styles.quantityContainer}>
+			<button
+				type="button"
+				onClick={() => handleUpdate(value - 1)}
+				disabled={isMinusDisabled}
+				className="btn border-0"
+			>
+				<span
+					className={clsx(
+						'material-icons  align-middle',
+						isMinusDisabled ? 'text-gray-200' : 'text-primary-400'
+					)}
+				>
+					remove
+				</span>
+			</button>
+			<input
+				type="number"
+				value={value}
+				onChange={handleInputChange}
+				className="form-control text-center border-0 p-0 mx-2"
+				readOnly
+			/>
+			<button
+				type="button"
+				onClick={() => handleUpdate(value + 1)}
+				disabled={isPlusDisabled}
+				className="btn border-0"
+			>
+				<span
+					className={clsx(
+						'material-icons  align-middle',
+						isPlusDisabled ? 'text-gray-200' : 'text-primary-400'
+					)}
+				>
+					add
+				</span>
+			</button>
+		</div>
+	)
+}
+
+export default QuantitySelector
