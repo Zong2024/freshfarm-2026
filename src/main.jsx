1451fd862bb82a51289482681c8dce8982c3dd5f
zong <Zong20021107@gmail.com>
2026-02-02 12:48:21 +0800

    feat(router): 新增路由設定，移除舊的App組件

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+import router from './router'
 import './assets/scss/main.scss'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-createRoot(document.getElementById('root')).render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
-)
+createRoot(document.getElementById('root')).render(<RouterProvider router={router} />)
---
+++ src/router/index.jsx
@@ -0,0 +1,28 @@
+import { createHashRouter } from 'react-router-dom'
+import Layout from '../components/Layout/Layout'
+import Home from '../pages/Home/Home'
+import ProductList from '../pages/ProductList'
+import Checkout from '../pages/Checkout/Checkout'
+
+const router = createHashRouter([
+	{
+		path: '/',
+		element: <Layout />,
+		children: [
+			{
+				index: true,
+				element: <Home />,
+			},
+			{
+				path: 'products',
+				element: <ProductList />,
+			},
+			{
+				path: 'checkout',
+				element: <Checkout />,
+			},
+		],
+	},
+])
+
+export default router
