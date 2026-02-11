447ae8945a2e4ec1535c664f5584f461a9743551
zong <Zong20021107@gmail.com>
2026-02-11 18:11:11 +0800

    feat(home): 動態載入首頁商品資料並提供載入骨架

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,8 @@ import ProductSection from './components/ProductSection/ProductSection'
 import { HOME_IMAGES } from '../../constants/images'
 import StorySection from './components/StorySection/StorySection'
 import BrandFeaturesSection from './components/BrandFeaturesSection/BrandFeaturesSection'
+import { getProducts } from '@/services/product.api'
+import { useEffect, useState } from 'react'
 const banners = [
 	{
 		id: 1,
@@ -37,48 +39,34 @@ const banners = [
 		},
 	},
 ]
-const products = [
-	{
-		id: 1,
-		name: '嚴選大樹老欉玉荷包',
-		origin: '古樂農場',
-		description:
-			'玉荷包、黑葉等品種在夏季上市，果肉Q彈甜中帶酸，風玉荷包、黑葉等玉荷包、黑葉等品種在夏季上市，果肉Q彈甜中帶酸，風玉荷包、黑葉等',
-		price: 729,
-		discountPrice: 683,
-		weight: 600,
-		unit: 'g',
-		img: 'https://github.com/Zong2024/freshfarm/blob/782bcd3365a6e7ea7dbce395488ad9591870eec8/assets/images/product/product-image-%E8%8D%94%E6%9E%9D.jpg?raw=true',
-	},
-	{
-		id: 2,
-		name: '有機綠竹筍(特級)4台',
-		origin: '農芒',
-		description:
-			'夏季是綠竹筍的產季口感清甜、脆嫩無論涼拌、煮湯都美夏季是綠竹筍的產季口感清甜、脆嫩無論涼拌、煮湯都美夏季是綠竹筍的產季口感清甜、脆嫩無論涼拌、煮湯都美',
-		price: 790,
-		discountPrice: null,
-		weight: 500,
-		unit: 'g',
-		img: 'https://github.com/Zong2024/freshfarm/blob/master/assets/images/product/product-image-%E7%B6%A0%E7%AB%B9%E7%AD%8D.jpg?raw=true',
-	},
-	{
-		id: 3,
-		name: '拉拉山洪家水蜜桃',
-		origin: '無花果農場',
-		description: '主要產於高山，果肉細緻柔嫩，香氣十足，入口即化',
-		price: 760,
-		discountPrice: 710,
-		weight: 800,
-		unit: 'g',
-		img: 'https://github.com/Zong2024/freshfarm/blob/master/assets/images/product/product-image-%E6%B0%B4%E8%9C%9C%E6%A1%83.jpg?raw=true',
-	},
-]
+
 const Home = () => {
+	const [products, setProducts] = useState([])
+
+	const [isLoading, setIsLoading] = useState(false)
+	useEffect(() => {
+		const fetchProducts = async () => {
+			setIsLoading(true)
+			try {
+				const res = await getProducts()
+				if (res.success && res.products) {
+					setProducts(res.products.slice(0, 5))
+					console.log(res.products.slice(0, 5))
+				} else {
+					console.error('API error', res.error)
+				}
+			} catch (error) {
+				console.error('讀取商品錯誤', error)
+			} finally {
+				setIsLoading(false)
+			}
+		}
+		fetchProducts()
+	}, [])
 	return (
 		<main>
 			<HeroSwiper banners={banners} />
-			<ProductSection products={products} />
+			<ProductSection products={products} isLoading={isLoading} />
 			<CategorySection />
 			<StorySection />
 			<BrandFeaturesSection />
