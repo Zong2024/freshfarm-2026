61f93b90ad95c2e605e9e967831290afda60c520
zong <Zong20021107@gmail.com>
2026-02-16 16:39:51 +0800

    feat(Navbar): 整合 AuthContext 實現登入/登出與狀態顯示

--- src/components/Layout/Navbar/Navbar.jsx
+++ src/components/Layout/Navbar/Navbar.jsx
@@ -1,11 +1,19 @@
-import { NavLink } from 'react-router-dom'
-import { useState } from 'react'
+import { NavLink, useNavigate } from 'react-router-dom'
 import styles from './Navbar.module.scss'
 import logo from '@/assets/images/logo.png'
 import { clsx } from 'clsx'
+import { useAuth } from '@/contexts/AuthContext'
 const Navbar = ({ isHomePage = { isHomePage } }) => {
-	// eslint-disable-next-line no-unused-vars
-	const [isSignIn, setIsSignIn] = useState(false) //還未新增邏輯
+	const { isAuth, logout, isLoading: isAuthLoading, user } = useAuth()
+	const navigate = useNavigate()
+
+	const handleLogout = () => {
+		logout()
+		navigate('/')
+	}
+
+	console.log(isAuth)
+
 	return (
 		<div className={clsx(styles.navBg)}>
 			<div
@@ -67,79 +75,100 @@ const Navbar = ({ isHomePage = { isHomePage } }) => {
 											<p className="text-gray-300">Cart</p>
 										</NavLink>
 									</li>
-									<li className="nav-item py-5 py-lg-0 dropdown">
-										{isSignIn ? (
-											<NavLink className="nav-link text-lg-center  p-0" to="/sign_in">
-												<span className="fs-lg-4 fs-5 fw-bold">登入/註冊</span>
-												<p className="text-gray-300">Sign in</p>
-											</NavLink>
-										) : (
-											<button
-												className="nav-link dropdown-toggle p-0 w-100 d-flex"
-												type="button"
-												data-bs-toggle="dropdown"
-												data-bs-display="static"
-												aria-expanded="false"
-											>
-												<div className="text-start text-lg-center">
-													<span className="fs-lg-4 fs-5 fw-bold">用戶名稱</span>
-													<p className="text-gray-300 ">Account</p>
-												</div>
-												<div className="align-self-center d-lg-none ms-auto">
-													<span className="material-icons ">keyboard_arrow_right</span>
-												</div>
-											</button>
-										)}
-
-										<ul
-											className={clsx(
-												'dropdown-menu border-0 mb-lg-2 rounded-3 p-4',
-												styles.dropdownShadow,
-												styles.customDropdown
+									{isAuthLoading ? (
+										<div className="spinner-border text-secondary "></div>
+									) : (
+										<li className="nav-item py-5 py-lg-0 dropdown">
+											{!isAuth ? (
+												<NavLink className="nav-link text-lg-center  p-0" to={navigate('/login')}>
+													<span className="fs-lg-4 fs-5 fw-bold">登入/註冊</span>
+													<p className="text-gray-300">Sign in</p>
+												</NavLink>
+											) : (
+												<button
+													className="nav-link dropdown-toggle p-0 w-100 d-flex"
+													type="button"
+													data-bs-toggle="dropdown"
+													data-bs-display="static"
+													aria-expanded="false"
+												>
+													<div className="text-start text-lg-center">
+														<span className="fs-lg-4 fs-5 fw-bold">{user}</span>
+														<p className="text-gray-300 ">Account</p>
+													</div>
+													<div className="align-self-center d-lg-none ms-auto">
+														<span className="material-icons ">keyboard_arrow_right</span>
+													</div>
+												</button>
 											)}
-											data-bs-display="static"
-										>
-											<h6 className="text-primary-400 mb-3 border-top border-black border-opacity-25 border-lg-0 pt-4 pt-lg-0">
-												會員中心
-											</h6>
-											<li>
-												<NavLink
-													className="dropdown-item  me-2 mb-3 mb-lg-2"
-													to="/user"
-													aria-hidden="true"
-												>
-													<span className="material-icons align-bottom me-2">account_circle</span>
-													我的帳戶
-												</NavLink>
-											</li>
-											<li>
-												<NavLink
-													className="dropdown-item  me-2 mb-3 mb-lg-2"
-													to="/orders"
-													aria-hidden="true"
-												>
-													<span className="material-icons align-bottom me-2">description</span>
-													訂單管理
-												</NavLink>
-											</li>
-											<li>
-												<NavLink
-													className="dropdown-item  me-2 mb-3 mb-lg-2"
-													to="/wishlist"
-													aria-hidden="true"
-												>
-													<span className="material-icons align-bottom me-2">favorite_border</span>
-													收藏清單
-												</NavLink>
-											</li>
-											<li>
-												<NavLink className="dropdown-item " to="/edit" aria-hidden="true">
-													<span className="material-icons align-bottom me-2">settings</span>
-													個人資料設定
-												</NavLink>
-											</li>
-										</ul>
-									</li>
+
+											<ul
+												className={clsx(
+													'dropdown-menu border-0 mb-lg-2 rounded-3 p-4',
+													styles.dropdownShadow,
+													styles.customDropdown
+												)}
+												data-bs-display="static"
+											>
+												<h6 className="text-primary-400 mb-3 border-top border-black border-opacity-25 border-lg-0 pt-4 pt-lg-0">
+													會員中心
+												</h6>
+												<li>
+													<NavLink
+														className="dropdown-item  me-2 mb-3 mb-lg-2"
+														to="/user"
+														aria-hidden="true"
+													>
+														<span className="material-icons align-bottom me-2">account_circle</span>
+														我的帳戶
+													</NavLink>
+												</li>
+												<li>
+													<NavLink
+														className="dropdown-item  me-2 mb-3 mb-lg-2"
+														to="/orders"
+														aria-hidden="true"
+													>
+														<span className="material-icons align-bottom me-2">description</span>
+														訂單管理
+													</NavLink>
+												</li>
+												<li>
+													<NavLink
+														className="dropdown-item  me-2 mb-3 mb-lg-2"
+														to="/wishlist"
+														aria-hidden="true"
+													>
+														<span className="material-icons align-bottom me-2">
+															favorite_border
+														</span>
+														收藏清單
+													</NavLink>
+												</li>
+												<li>
+													<NavLink
+														className="dropdown-item me-2 mb-3 mb-lg-2"
+														to="/edit"
+														aria-hidden="true"
+													>
+														<span className="material-icons align-bottom me-2">settings</span>
+														個人資料設定
+													</NavLink>
+												</li>
+												<li>
+													<button
+														type="button"
+														className="dropdown-item me-2 mb-3 mb-lg-2"
+														aria-hidden="true"
+														onClick={handleLogout}
+													>
+														<span className="material-icons align-bottom me-2">logout</span>
+														登出
+													</button>
+												</li>
+											</ul>
+										</li>
+									)}
 								</ul>
 							</div>
 						</div>
