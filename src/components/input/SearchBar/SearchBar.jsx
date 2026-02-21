a4138368d61e23e4b391d6f8ba2d784ed45385b3
Xiu <lynn202400095@gmail.com>
2026-02-21 14:58:02 +0800

    加入收尋功能

--- src/components/input/SearchBar/SearchBar.jsx
+++ src/components/input/SearchBar/SearchBar.jsx
@@ -1,11 +1,23 @@
 import { clsx } from 'clsx'
 import styles from './SearchBar.module.scss'
-const SearchBar = ({ isHome = false }) => {
+const SearchBar = ({ keyword, setKeyword, onSearch, isHome = false }) => {
+	const handleKeyDown = e => {
+		if (e.key === 'Enter') {
+			onSearch()
+		}
+	}
 	return (
 		<div className={clsx(styles.searchBox, isHome && styles.home)}>
 			<div className={clsx(styles.customSearchBar)}>
-				<input type="text" className={clsx(styles.searchInput)} placeholder="請輸入商品名稱…" />
-				<button type="button" className={clsx(styles.searchBtn)}>
+				<input
+					type="search"
+					className={clsx(styles.searchInput)}
+					placeholder="請輸入商品名稱…"
+					value={keyword}
+					onChange={e => setKeyword(e.target.value)}
+					onKeyDown={handleKeyDown}
+				/>
+				<button type="button" className={clsx(styles.searchBtn)} onClick={onSearch}>
 					<span className={clsx('material-icons text-primary', styles.searchBtnIcon)}>search</span>
 				</button>
 			</div>
--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -2,12 +2,16 @@ import Compare from '../../components/Layout/Compare/Compare'
 import ProductCategory from './components/ProductCategory/ProductCategory'
 import ProductsSection from './components/ProductsSection/ProductsSection'
 import { useCallback, useEffect, useState } from 'react'
-import { getProducts } from '@/services/product.api'
+import { getAllProducts, getProducts } from '@/services/product.api'
 
 const ProductList = () => {
 	const [products, setProducts] = useState([])
 	const [pagination, setPagination] = useState({})
 	const [isLoading, setIsLoading] = useState(false)
+	const [filteredProducts, setFilteredProducts] = useState([])
+	const [currentPage, setCurrentPage] = useState(1)
+	const productsPerPage = 9
+	const [keyword, setKeyword] = useState('')
 
 	const getProductsApi = useCallback(async (page = 1) => {
 		setIsLoading(true)
@@ -27,9 +31,37 @@ const ProductList = () => {
 	}, [])
 
 	useEffect(() => {
-		getProductsApi()
-	}, [getProductsApi])
+		if (!keyword) {
+			getProductsApi(currentPage)
+		}
+	}, [keyword, currentPage, getProductsApi])
 
+	const handleSearch = async () => {
+		if (!keyword) return
+		setIsLoading(true)
+		try {
+			//抓全部商品
+			const result = await getAllProducts()
+			if (result.success) {
+				const all = Object.values(result.products)
+				const filter = all.filter(item => item.title.includes(keyword))
+				setFilteredProducts(filter)
+				setCurrentPage(1)
+			}
+		} catch (error) {
+			console.error(error)
+		} finally {
+			setIsLoading(false)
+		}
+	}
+	//目前要顯示的商品
+	const displayProducts = keyword
+		? filteredProducts.slice((currentPage - 1) * productsPerPage, currentPage * productsPerPage)
+		: products
+	const totalPages =
+		keyword && filteredProducts.length > 0
+			? Math.ceil(filteredProducts.length / productsPerPage)
+			: pagination.total_pages || 1
 	return (
 		<main>
 			{/*商品list*/}
@@ -37,10 +69,14 @@ const ProductList = () => {
 				<div className="row">
 					<ProductCategory />
 					<ProductsSection
-						products={products}
-						pagination={pagination}
-						changePage={getProductsApi}
+						products={displayProducts}
+						pagination={{ ...pagination, total_pages: totalPages }}
+						changePage={setCurrentPage}
 						isLoading={isLoading}
+						keyword={keyword}
+						setKeyword={setKeyword}
+						onSearch={handleSearch}
+						currentPage={currentPage}
 					/>
 				</div>
 			</section>
