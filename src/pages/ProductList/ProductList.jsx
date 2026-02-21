2b3067fc639266249ca2859c026b4105b02d07e0
Xiu <lynn202400095@gmail.com>
2026-02-21 17:29:25 +0800

    首頁搜尋跳轉至產品搜尋

--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -3,15 +3,19 @@ import ProductCategory from './components/ProductCategory/ProductCategory'
 import ProductsSection from './components/ProductsSection/ProductsSection'
 import { useCallback, useEffect, useState } from 'react'
 import { getAllProducts, getProducts } from '@/services/product.api'
+import { useSearchParams } from 'react-router-dom'
 
 const ProductList = () => {
+	const [searchParams] = useSearchParams()
+	const urlKeyword = searchParams.get('keyword') || '' // 首頁搜尋跳轉的 keyword
+
 	const [products, setProducts] = useState([])
 	const [pagination, setPagination] = useState({})
 	const [isLoading, setIsLoading] = useState(false)
-	const [filteredProducts, setFilteredProducts] = useState([])
+	const [filteredProducts, setFilteredProducts] = useState([]) // 搜尋結果
 	const [currentPage, setCurrentPage] = useState(1)
 	const productsPerPage = 9
-	const [keyword, setKeyword] = useState('')
+	const [keyword, setKeyword] = useState('') // 產品頁內部搜尋
 
 	const getProductsApi = useCallback(async (page = 1) => {
 		setIsLoading(true)
@@ -29,22 +33,31 @@ const ProductList = () => {
 			setIsLoading(false)
 		}
 	}, [])
+	//首次進入頁面判斷是否來自首頁搜尋
+	useEffect(() => {
+		if (urlKeyword) {
+			setKeyword(urlKeyword)
+			handleSearch(urlKeyword)
+		} else {
+			getProductsApi(currentPage)
+		}
+	}, [])
 
 	useEffect(() => {
 		if (!keyword) {
 			getProductsApi(currentPage)
 		}
-	}, [keyword, currentPage, getProductsApi])
+	}, [currentPage, keyword, getProductsApi])
 
-	const handleSearch = async () => {
-		if (!keyword) return
+	const handleSearch = async (searchValue = keyword) => {
+		if (!searchValue.trim()) return
 		setIsLoading(true)
 		try {
 			//抓全部商品
 			const result = await getAllProducts()
 			if (result.success) {
 				const all = Object.values(result.products)
-				const filter = all.filter(item => item.title.includes(keyword))
+				const filter = all.filter(item => item.title.includes(searchValue))
 				setFilteredProducts(filter)
 				setCurrentPage(1)
 			}
@@ -75,7 +88,7 @@ const ProductList = () => {
 						isLoading={isLoading}
 						keyword={keyword}
 						setKeyword={setKeyword}
-						onSearch={handleSearch}
+						onSearch={() => handleSearch(keyword)}
 						currentPage={currentPage}
 					/>
 				</div>
