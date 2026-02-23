5996647062090b98c2577f8e6bdc45c6eb3b8bf9
zong <Zong20021107@gmail.com>
2026-02-23 17:24:19 +0800

    feat(product-cart): 新增產品加入購物車按鈕的載入狀況
    新增disborad 、 setTimeout 避免用戶重複點選按鈕導致api崩潰

--- src/components/button/CartButton.jsx
+++ src/components/button/CartButton.jsx
@@ -1,9 +1,20 @@
 import BaseButton from './BaseButton'
 import styles from './Button.module.scss'
-const CartButton = ({ onClick }) => {
+const CartButton = ({ onClick, isLoading }) => {
 	return (
-		<BaseButton className={styles.cartButton} onClick={onClick}>
-			<h6>加入購物車</h6>
+		<BaseButton className={styles.cartButton} onClick={onClick} disabled={isLoading}>
+			{isLoading ? (
+				<>
+					<span
+						className="spinner-border spinner-border-sm"
+						role="status"
+						aria-hidden="true"
+					></span>
+					加入中...
+				</>
+			) : (
+				<h6>加入購物車</h6>
+			)}
 		</BaseButton>
 	)
 }
