a28205dc0209c98809ab167e8aa5b88abd1db135
zong <Zong20021107@gmail.com>
2026-02-23 14:19:32 +0800

    feat(checkout): 更新結帳步驟名稱與內容

--- src/pages/Checkout/components/StepperSection/StepperSection.jsx
+++ src/pages/Checkout/components/StepperSection/StepperSection.jsx
@@ -6,17 +6,17 @@ const StepperSection = ({ step }) => {
 		<div className={styles.step}>
 			<div className={styles.stepItem}>
 				<div className={clsx(styles.stepCircle, { [styles.active]: step >= 1 })}>1</div>
-				<p className={styles.stepText}>確認訂單明細</p>
+				<p className={styles.stepText}>購物車</p>
 			</div>
 			<div className={styles.stepLine}></div>
 			<div className={styles.stepItem}>
 				<div className={clsx(styles.stepCircle, { [styles.active]: step >= 2 })}>2</div>
-				<p className={styles.stepText}>選擇付款方式</p>
+				<p className={styles.stepText}>選擇配送與付款方式</p>
 			</div>
 			<div className={styles.stepLine}></div>
 			<div className={styles.stepItem}>
 				<div className={clsx(styles.stepCircle, { [styles.active]: step >= 3 })}>3</div>
-				<p className={styles.stepText}>完成訂購</p>
+				<p className={styles.stepText}>提交訂單與付款</p>
 			</div>
 		</div>
 	)
