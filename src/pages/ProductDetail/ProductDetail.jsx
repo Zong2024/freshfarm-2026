b691078596b2fb49b34dbabcc46082b5f9427826
Xiu <lynn202400095@gmail.com>
2026-03-15 22:30:03 +0800

    移除商品詳情頁的col-12

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -199,12 +199,12 @@ const ProductDetail = () => {
 						<div className="col-12 text-center mb-7 mb-lg-8">
 							<h3 className="fs-lg-2">小農介紹</h3>
 						</div>
-						<div className="col-12 col-lg-4 align-self-center">
+						<div className="col-lg-4 align-self-center">
 							<div className={styles.farmerImage}>
 								<img src={farmer} alt="小農圖片" />
 							</div>
 						</div>
-						<div className="col-12 col-lg-7">
+						<div className="col-lg-7">
 							<div className="p-4">
 								<h6 className="fs-lg-4 mb-2 text-primary-400">繼承父親四十年麻豆大白柚</h6>
 								<h5 className="fs-lg-2 mb-3 mb-lg-6">兩代草生栽培珍惜土地</h5>
